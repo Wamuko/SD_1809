13263ec09d610f22661f179cc3107ee6f5cf7d4e
Kentaro Hoshikawa <kentaro_hoshikawa@shino.ecei.tohoku.ac.jp>
2018-10-20 17:04:24 +0900

    update: update PlantAnimator

--- whisper/PlantAnimator.py
+++ whisper/PlantAnimator.py
@@ -9,6 +9,9 @@ class PlantAnimator:
         self.user_data = user_data
         self.__plant = None
 
+    def create_plant(self, ):
+        pass
+
     # 要求された名前から対応する植物を再生します
     def connect(self, name):
         self.__plant = self.user_data.reanimate_plant(name)
@@ -17,3 +20,7 @@ class PlantAnimator:
     def disconnect(self):
         self.__plant = None
         pass
+
+    # Lineのテキストを植物に伝え、応答を受け取ります
+    def communicate(self, text):
+        return self.__plant.chat(text)
