052c0e142c1d8008ab621e5c282747680107dc9f
Wamuko <wamuko.kazu@gmail.com>
2018-10-21 12:48:32 +0900

    app.pyの方でよりPlantAnimatorに頼った方向に修正

--- whisper/Plant.py
+++ whisper/Plant.py
@@ -17,7 +17,7 @@ class Plant:
                 luminosity_threshold=LuminosityThreshold,
                 temperture_min_relax=TempertureMinRelax,
                 temperture_max_relax=TempertureMaxRelax,
-                listen_bieacon=(None, 0),):
+                listen_beacon=(None, 0),):
         self.display_name = display_name
         self.name = name
         self.__sensor_buf = sensor_buffer
@@ -26,14 +26,14 @@ class Plant:
         self.luminosity_threshold = luminosity_threshold
         self.temperture_min_relax = temperture_min_relax
         self.temperture_max_relax = temperture_max_relax
-        self.listen_bieacon = listen_bieacon
+        self.listen_beacon = listen_beacon
 
         executor = ThreadPoolExecutor(2)
         self.__listening_thread = executor.submit(self.__sensor_buf.start)
 
     # ここでlisten_boecon(ビーコンが反応してから diff 現時刻, 設定がOn)なら1時間スパンにする(4時間の間)
     def update(self):
-        if 14401 < (datetime.now().strftime('%s') - self.listen_bieacon[0]).strftime('%s') and self.listen_bieacon[1] is 1:
+        if 14401 < (datetime.now().strftime('%s') - self.listen_beacon[0]).strftime('%s') and self.listen_beacon[1] is 1:
             self.__sensor_buf.fetch_span = 7200
         else:
             self.__sensor_buf.fetch_span = 600
