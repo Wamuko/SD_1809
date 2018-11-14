b5278b72e9423194e2c9f67063eff11fd3ab5367
Kentaro Hoshikawa <kentaro_hoshikawa@shino.ecei.tohoku.ac.jp>
2018-11-14 16:42:55 +0900

    update: Pseudowater

--- whisper/PseudoPlant.py
+++ whisper/PseudoPlant.py
@@ -6,11 +6,17 @@ class PseudoPlant(Plant):
         self.name = name
         self.__speech_center = speech_center
         self.dry = True
-        self.bright = True
+        self.dark = True
 
     def report_wether_forecast(self, postal_code):
         return self.__speech_center.report_wether_forecast(postal_code)
 
-    
+    def needWater(self):
+        return self.dry
+
+    def needLuminesity(self):
+        return self.dark
+
+        
 
     
